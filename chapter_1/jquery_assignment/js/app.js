4a9b3dd402eb4edac2cab07f7a67f5a4b38c549c
Caio Andrade <CaioAndrade@MacBook-Pro-de-Caio.local>
2017-06-23 16:58:10 -0400

    Finished assignment #2 - DOM manipulation

--- chapter_1/jquery_assignment/js/app.js
+++ chapter_1/jquery_assignment/js/app.js
@@ -1,1 +1,38 @@
 //Write your jQuery code in this file
+$("#edit-header").html("The jQuery Challenge");
+
+// change color of box1
+$("#box1").css({"background": "green"});
+
+// change font-weight and the font-size of the text
+$("#para1").css({"font-size": "x-large", "font-weight": "bold;"});
+
+// make the box spin
+$(".box2").addClass("spin");
+
+// create alert after click
+$( "#button1" ).click(function afterClick() {
+  alert( "Hi, Alice!!!" );
+});
+
+// change color of box on hover
+$("#box3").hover(
+  function hoverBox() {
+    $("#box3").css({"background": "green"});
+  }, function() {
+    $("#box3").css({"background": "red"});
+  }
+);
+
+// console log the value inside the input box
+$("#input1").keyup(function() {
+  console.log($("#input1").val());
+});
+
+// move box
+$("#box4").animate({
+    top: "+=149", // subtracted 1px to make sure
+    left: "+=399" // the box doesn't leave the boundaries
+  }, {
+    duration: 1200,
+    });
